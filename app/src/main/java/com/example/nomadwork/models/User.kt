9a37b82dd080e4b2e2c1c32b55c4fd1a01822f54
flavio <henrique06>
2019-11-04 16:17:36 -0300

    Refactoring: User method(get);

--- app/src/main/java/com/example/nomadwork/models/User.kt
+++ app/src/main/java/com/example/nomadwork/models/User.kt
@@ -3,10 +3,28 @@ package com.example.nomadwork.models
 import com.google.gson.annotations.SerializedName
 
 data class User(
-    @SerializedName("nome") val name: String,
+    @SerializedName("name") val name: String,
     @SerializedName("email") val email: String,
-    @SerializedName("idade") val idade: Int,
-    @SerializedName("sexo") val sexo: String
+    @SerializedName("dateborn") val age: String,
+    @SerializedName("gender") val gender: String
 ){
+    val getName: String
+        get() {
+            return name
+        }
 
+    val getEmail: String
+        get() {
+            return email
+        }
+
+    val getAge: String
+        get() {
+            return age
+        }
+
+    val getGender: String
+        get() {
+            return gender
+        }
 }
