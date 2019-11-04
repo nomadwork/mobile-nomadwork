7043c984729123e422e757f5793c8198e7c8ac7e
flavio <henrique06>
2019-11-04 16:11:27 -0300

    Added: code on BaseResponse;

--- app/src/main/java/com/example/nomadwork/API/Models/BaseResponse.kt
+++ app/src/main/java/com/example/nomadwork/API/Models/BaseResponse.kt
@@ -4,6 +4,7 @@ import com.example.nomadwork.models.WorkStation
 import com.google.gson.annotations.SerializedName
 
 data class BaseResponse<T> (
+    @SerializedName("code") val code: Int,
     @SerializedName("message") val message: String,
     @SerializedName("result") val result: T
 )
