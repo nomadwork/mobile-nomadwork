f574936f8f300c796210c96cb5e5cb8180a009db
flavio <henrique06>
2019-11-04 16:10:39 -0300

    Added: searchList, userMenu and Session(token);

--- app/src/main/java/com/example/nomadwork/Activity/MapsActivity.kt
+++ app/src/main/java/com/example/nomadwork/Activity/MapsActivity.kt
@@ -13,21 +13,24 @@ import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.view.*
-import android.widget.LinearLayout
-import android.widget.SearchView
-import android.widget.Toast
+import android.widget.*
 import androidx.core.app.ActivityCompat
 import androidx.viewpager.widget.ViewPager
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
 import com.example.nomadwork.API.NomadWorkAPIService
-import com.example.nomadwork.API.request.WorkStationRequest
-import com.example.nomadwork.API.response.WSSuggestResponse
+import com.example.nomadwork.API.Session
 import com.example.nomadwork.Adapters.WSDetailsImagesAdapter
+import com.example.nomadwork.Adapters.WSListAdapter
 import com.example.nomadwork.Fragments.LoginFragment
 import com.example.nomadwork.Fragments.NewWorkStationFragment
+import com.example.nomadwork.Fragments.UserMenu
 import com.example.nomadwork.Helpers.AndroidDisposable
 import com.example.nomadwork.Helpers.LocationHelper
 import com.example.nomadwork.Helpers.WorkStationHelper
 import com.example.nomadwork.R
+import com.example.nomadwork.models.WorkStation
 import com.example.nomadwork.models.WorkStationDetails
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
@@ -38,7 +41,6 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.bottom_sheet.*
-import kotlinx.android.synthetic.main.fragment_new_work_station.*
 import retrofit2.HttpException
 import com.google.android.material.floatingactionbutton.FloatingActionButton as FloatingActionButton1
 
@@ -52,14 +54,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mLocationRequest: LocationRequest
     private val distanceToNewMarkers = 30 //meters
     private var centerMapLocation: Location? = null
-
-
+    @BindView(R.id.ws_filter_layout) lateinit var wsFilter: View
+    @BindView(R.id.ws_list_view) lateinit var wsList: ListView
+    @BindView(R.id.simpleSearchView) lateinit var searchEdit: SearchView
+    @BindView(R.id.button_ws_register) lateinit var buttonWSRegister: FloatingActionButton1
+    @BindView(R.id.menuButtom) lateinit var menuButton: Button
 
     private lateinit var sheetBehavior: BottomSheetBehavior<LinearLayout>
 
-    private lateinit var viewpager : ViewPager
-
-    private lateinit var searchEdit: SearchView
+    private lateinit var viewpager: ViewPager
 
     companion object {
         const val TAG = "MapsActivity"
@@ -67,8 +70,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initLocation()
         setContentView(R.layout.activity_maps)
+        ButterKnife.bind(this)
+        initLocation()
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -78,18 +82,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
 
-        searchEdit = findViewById(R.id.simpleSearchView)
+        searchEdit.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (searchEdit.query.toString() == "") toggleFilterWS(wsFilter)
+                return false
+            }
 
-
-
-        val buttonWSRegister = findViewById<FloatingActionButton1>(R.id.button_ws_register)
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (searchEdit.query.toString() != "") {
+                    getWSNames()
+                }
+                return false
+            }
+        })
 
         buttonWSRegister.setOnClickListener {
             val fragment = NewWorkStationFragment()
 
             val t = supportFragmentManager.beginTransaction()
-            t.setCustomAnimations(R.anim.enter_from_right,
-                R.anim.exit_to_left)
+            t.setCustomAnimations(
+                R.anim.enter_from_right,
+                R.anim.exit_to_left
+            )
             t.replace(R.id.fragments_holder_maps, fragment, "NewWorkStation")
             t.commit()
         }
@@ -104,7 +118,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onResume() {
         super.onResume()
-        if(checkPermissionForLocation(this)){
+        if (checkPermissionForLocation(this)) {
             startLocationUpdates()
             System.out.println(LocationHelper.getUserLocation()?.latitude) //TODO: remove this line!
             System.out.println("INICIOU O SERVICO!!!!") //TODO: remove this line!
@@ -118,26 +132,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        return if(sheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED && event!!.keyCode != 8){ //KeyCode == 8 is BackSpace on keyboard.
+        return if (sheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED && event!!.keyCode != 8) { //KeyCode == 8 is BackSpace on keyboard.
             expandCloseSheet()
             false
-        } else if(supportFragmentManager.findFragmentByTag("NewWorkStation") != null){
-                val fragment = supportFragmentManager.findFragmentByTag("NewWorkStation")
-                supportFragmentManager.beginTransaction().remove(fragment!!).commit()
-                false
-            } else {
+        } else if (supportFragmentManager.findFragmentByTag("NewWorkStation") != null) {
+            val fragment = supportFragmentManager.findFragmentByTag("NewWorkStation")
+            supportFragmentManager.beginTransaction().remove(fragment!!).commit()
+            false
+        } else if (supportFragmentManager.findFragmentByTag("UserMenu") != null) {
+            closeUserMenuFromActivity()
+            false
+        } else if (wsFilter.visibility == View.VISIBLE){
+            wsFilter.visibility = View.GONE
+            false
+        } else {
             logout()
             true
         }
         //return super.onKeyDown(keyCode, event)
     }
 
+    @SuppressLint("RestrictedApi")
+    @OnClick(R.id.menuButtom)
+    fun openMenu(){
+        val fragment = UserMenu()
+
+        buttonWSRegister.visibility = View.GONE
+        menuButton.visibility = View.GONE
+
+        val t = supportFragmentManager.beginTransaction()
+        t.setCustomAnimations(
+            R.anim.enter_from_left,
+            R.anim.exit_to_right
+        )
+        t.replace(R.id.fragments_holder_maps, fragment, "UserMenu")
+        t.commit()
+    }
+
     fun logout() {
         WorkStationHelper.clearListWS()
+        Session.get(null)?.logout(this)
         val i = Intent(this, LoginActivity::class.java)
         i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         this.startActivity(i)
         this.finish()
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun closeUserMenuFromActivity(){
+        val fragment = supportFragmentManager.findFragmentByTag("UserMenu")
+        val t = supportFragmentManager.beginTransaction()
+        t.setCustomAnimations(
+            R.anim.enter_from_right,
+            R.anim.exit_to_left
+        )
+        t.remove(fragment!!).commit()
+
+        buttonWSRegister.visibility = View.VISIBLE
+        menuButton.visibility = View.VISIBLE
     }
 
     private fun expandCloseSheet() {
@@ -147,7 +199,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             return
         }
 
-        if(sheetBehavior.state != BottomSheetBehavior.STATE_HIDDEN) {
+        if (sheetBehavior.state != BottomSheetBehavior.STATE_HIDDEN) {
             bottom_sheet.visibility = View.VISIBLE
             return
         } else {
@@ -156,7 +208,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun bottomSheetCallback(){
+    private fun bottomSheetCallback() {
         /**
          * bottom sheet state change listener
          * we are changing button text when sheet changed state
@@ -190,9 +242,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         })*/
     }
 
-    private fun bottomSheetClickListener(){
+    private fun bottomSheetClickListener() {
         bottom_sheet.setOnTouchListener { p0, event ->
-            if(event!!.action == MotionEvent.ACTION_UP){
+            if (event!!.action == MotionEvent.ACTION_UP) {
                 sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
             }
             true
@@ -231,8 +283,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         mMap.setOnMarkerClickListener(GoogleMap.OnMarkerClickListener {
-            if(WorkStationHelper.checkInitialized()){
-                val mark = WorkStationHelper.getWSByPosition(it.position.latitude, it.position.longitude)
+            if (WorkStationHelper.checkInitialized()) {
+                val mark =
+                    WorkStationHelper.getWSByPosition(it.position.latitude, it.position.longitude)
 
                 val api = NomadWorkAPIService.api()
                 val r = api.workStatinDetails.workStationDetails(mark!!.workStationId)
@@ -240,13 +293,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 val subscription = r.observeOn(AndroidSchedulers.mainThread())
                     .subscribeOn(Schedulers.io())
                     .subscribe({ result ->
+                        // Invalid access_token. Force logout and login
+                        val s = Session.get(null)
+                        if (result.code == 401 && s != null) {
+                            logout()
+                        }
                         showWSDetails(result.result)
                     }, { error ->
                         if (error is HttpException) {
                             Toast.makeText(this, error.message!!, Toast.LENGTH_SHORT).show()
                             Log.e(LoginFragment.TAG, error.message!!)
                         } else {
-                            Toast.makeText(this, getString(R.string.login_error_contact_admin), Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this,
+                                getString(R.string.login_error_contact_admin),
+                                Toast.LENGTH_SHORT
+                            ).show()
                             Log.e(LoginFragment.TAG, error.message!!)
                         }
                     })
@@ -262,14 +324,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         })
 
         mMap.setOnMapClickListener {
-            /*System.out.println(supportFragmentManager.findFragmentByTag("details"))
-            if(supportFragmentManager.fragments.isNotEmpty() && supportFragmentManager.findFragmentByTag("details") != null){
-                val fragment = supportFragmentManager.findFragmentByTag("details")
-                val f = supportFragmentManager.beginTransaction()
-                f.setCustomAnimations(R.anim.enter_from_bottom,
-                    R.anim.exit_to_bottom)
-                f.remove(fragment!!).commit()
-            }*/
 
             if(supportFragmentManager.findFragmentByTag("NewWorkStation") != null){
                 //val fragment = supportFragmentManager.findFragmentByTag("NewWorkStation")
@@ -277,8 +331,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 return@setOnMapClickListener
             }
 
-            if(sheetBehavior.state != BottomSheetBehavior.STATE_HIDDEN){
+            if (sheetBehavior.state != BottomSheetBehavior.STATE_HIDDEN) {
                 expandCloseSheet()
+            }
+
+
+            if (supportFragmentManager.findFragmentByTag("UserMenu") != null) {
+                closeUserMenuFromActivity()
+                return@setOnMapClickListener
             }
         }
 
@@ -291,10 +351,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
                 //val myLocation = LatLng(LocationHelper.getUserLocation()!!.latitude, LocationHelper.getUserLocation()!!.longitude)
                 //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 16.0f))
-                getMarkersByLocation(mMap.projection.fromScreenLocation(Point()).latitude, mMap.projection.fromScreenLocation(Point()).longitude) {
-                    addMarkersByLocation()
+                if(WorkStationHelper.checkInitialized()) {
+                    getMarkersByLocation(
+                        mMap.projection.fromScreenLocation(Point()).latitude,
+                        mMap.projection.fromScreenLocation(Point()).longitude
+                    ) {
+                        addMarkersByLocation()
+                    }
+                    Log.i(TAG, "New Markers")
                 }
-                Log.i(TAG, "New Markers")
             } else {
                 getMarkersByLocation(LocationHelper.getUserLocation()!!.latitude, LocationHelper.getUserLocation()!!.longitude) {
                     addMarkersByLocation()
@@ -360,7 +425,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         ) {
             return
         }
-        mFusedLocationProviderClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+        mFusedLocationProviderClient?.requestLocationUpdates(
+            mLocationRequest,
+            mLocationCallback,
+            Looper.myLooper()
+        )
     }
 
     private val mLocationCallback = object : LocationCallback() {
@@ -431,11 +500,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun getMarkerDetails(){
+    private fun getMarkerDetails() {
 
     }
 
-    private fun getMarkersByLocation(lat: Double, long: Double, callback: () -> Unit){
+    private fun getMarkersByLocation(lat: Double, long: Double, callback: () -> Unit) {
 
         val api = NomadWorkAPIService.api()
 
@@ -447,6 +516,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val subscription = r.observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
             .subscribe({ result ->
+                // Invalid access_token. Force logout and login
+                val s = Session.get(null)
+                if (result.code == 401 && s != null) {
+                    logout()
+                }
                 mMap.clear()
                 WorkStationHelper.initListWS(result.result)
                 callback()
@@ -497,5 +571,99 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         tab_layout.setupWithViewPager(viewpager, true)
 
         expandCloseSheet()
+    }
+
+    private fun toggleFilterWS(layout: View) {
+        val isOpen = layout.visibility == View.VISIBLE
+
+        if (isOpen) {
+            layout.animate().alpha(0.0f).withEndAction {
+                layout.visibility = View.GONE
+            }.start()
+        } else {
+            layout.alpha = 0.0f
+            layout.visibility = View.VISIBLE
+
+            layout.animate().alpha(1.0f)
+        }
+    }
+
+    private fun buildListWS(list: List<WorkStation>) {
+
+        val list = ArrayList(list)
+
+        val adapter = WSListAdapter(applicationContext!!, list)
+        wsList.adapter = adapter
+
+        wsList.setOnItemClickListener { parent, _, position, _ ->
+            val mark = list[position]
+
+            val wsLocation = LatLng(list[position].workStationLocation.lat, list[position].workStationLocation.long)
+
+            val api = NomadWorkAPIService.api()
+            val r = api.workStatinDetails.workStationDetails(mark.workStationId)
+
+            val subscription = r.observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ result ->
+                    // Invalid access_token. Force logout and login
+                    val s = Session.get(null)
+                    if (result.code == 401 && s != null) {
+                        logout()
+                    }
+                    //TODO: refazer marcacao. Nao esta funcionando
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wsLocation, 15.5f))
+                    mMap.addMarker(MarkerOptions().position(wsLocation).title(mark.workStationName).icon(
+                        BitmapDescriptorFactory.fromResource(R.drawable.ws_pin))).showInfoWindow()
+                    showWSDetails(result.result)
+                    toggleFilterWS(wsFilter)
+                }, { error ->
+                    if (error is HttpException) {
+                        Toast.makeText(this, error.message!!, Toast.LENGTH_SHORT).show()
+                        Log.e(LoginFragment.TAG, error.message!!)
+                    } else {
+                        Toast.makeText(
+                            this,
+                            getString(R.string.login_error_contact_admin),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Log.e(LoginFragment.TAG, error.message!!)
+                    }
+                })
+            disposable.add(subscription)
+
+        }
+    }
+
+    fun getWSNames(){
+        val api = NomadWorkAPIService.api()
+
+        val r = api.workStationSeacrhList.workStationSearchList(searchEdit.query.toString())
+
+        val subscription = r.observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({ result ->
+                // Invalid access_token. Force logout and login
+                val s = Session.get(null)
+                if (result.code == 401 && s != null) {
+                    logout()
+                }
+                if(result.code == 404){
+                    Toast.makeText(this, result.message!!, Toast.LENGTH_SHORT).show()
+                }
+                if(result.code == 200) {
+                    buildListWS(result.result)
+                    toggleFilterWS(wsFilter)
+                }
+            }, { error ->
+                if (error is HttpException) {
+                    Toast.makeText(this, error.message!!, Toast.LENGTH_SHORT).show()
+                    Log.e(LoginFragment.TAG, error.message!!)
+                } else {
+                    Toast.makeText(this, getString(R.string.login_error_contact_admin), Toast.LENGTH_SHORT).show()
+                    Log.e(LoginFragment.TAG, error.message!!)
+                }
+            })
+        disposable.add(subscription)
     }
 }
