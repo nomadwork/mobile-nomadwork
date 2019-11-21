d3a82197037a4233ef8c173bdae0aa17fe28412a
flavio <henrique06>
2019-11-21 12:18:42 -0300

    Create: Constants class;

---
+++ app/src/main/java/com/example/nomadwork/Helpers/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.nomadwork.Helpers
+
+class Constants {
+    companion object {
+        const val USER_PHOTO = "USER_PHOTO"
+        const val USER_EMAIL = "USER_EMAIL"
+    }
+}
